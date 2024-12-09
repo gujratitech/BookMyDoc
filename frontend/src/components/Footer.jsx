63d0fafd9a9be01a136198c7645abfb259b5c534
Vasu Parashar <vasuparashar18@gmail.com>
2024-12-09 12:16:37 +0530

    Made changes to contact and footer

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -24,8 +24,8 @@ const Footer = () => {
         <div>
           <p className='text-xl font-medium mb-5'>GET IN TOUCH</p>
           <ul className='flex flex-col gap-2 text-gray-600'>
-            <li>+91-9548938557</li>
-            <li>ashurajput7017@gmail.com</li>
+            <li>+91-8468938745</li>
+            <li>vasuparashar18@gmail.com</li>
           </ul>
         </div>
 
